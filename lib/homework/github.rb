35a8a7837dbd8bd3c94422bc2323fb31541f3535
Pat McAleavey <pmcaleavey8@gmail.com>
2016-03-09 15:51:45 -0500

    Issue Tracking Homework Completed

--- lib/homework/github.rb
+++ lib/homework/github.rb
@@ -4,7 +4,7 @@ module Homework
     base_uri "https://api.github.com"
 
     def initialize
-      @auth_token = "a8ba164f9526540607f9a91b0625120ff5352f3e"
+      @auth_token = "f8fc5b09763cfdb64e6a6cd3a25d2805d7f1d303"
       @headers = {
         "Authorization" => "token #{@auth_token}",
         "User-Agent"    => "HTTParty"
@@ -27,6 +27,22 @@ module Homework
 
     def list_team_members(team_id)
       Github.get("/teams/#{team_id}/members", headers: @headers)
+    end
+
+    def list_issues(owner, repo)
+      Github.get("/repos/#{owner}/#{repo}/issues", headers: @headers)
+    end
+
+    #PATCH /repos/:owner/:repo/issues/:number
+    def close_an_issue(owner, repo, number)
+      Github.patch("/repos/#{owner}/#{repo}/issues/#{number}", headers: @headers,
+        body: {state: "closed" }.to_json)
+    end
+
+    #POST /repos/:owner/:repo/issues/:number/comments
+    def comment_on_issue(owner, repo, number)
+      Github.post("/repos/#{owner}/#{repo}/issues/#{number}/comments", headers: @headers,
+        body: {body: "Comments on Test 2"}.to_json)
     end
   end
 end
